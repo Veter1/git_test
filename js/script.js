914bf4163bdac11db4b0c1692cae0fde40d3a513
Kamikoto <danil.jacque.venus@gmail.com>
2023-06-29 21:30:50 +0300

    q
    q

--- js/script.js
+++ js/script.js
@@ -4,11 +4,16 @@ let body = document.body;
 let main_el = body.querySelectorAll('main')[0];
 let circles = body.querySelectorAll('.circles');
 let sections = body.querySelectorAll('.sections');
+let menu = body.querySelectorAll('.nav_panel');
+let menu_buttons = body.querySelectorAll('.nav_item');
 
 let curent_section = 0;
 let touchstart_y = 0;
 let permission = true;
 let circle_height_koef = 0.8;
+
+if (window.innerHeight / window.innerWidth > 1)
+    circle_height_koef = 0.4;
 
 // установление своего vh
 let vh = window.innerHeight * 0.01;
@@ -17,10 +22,8 @@ window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   });
-// -----------------------------
+// ----------------------------- 
 
-if (window.innerHeight / window.innerWidth > 1)
-    circle_height_koef = 0.4;
 
 /*                  тестові бульбашки
 document.getElementById('bubels').animate([
@@ -34,7 +37,6 @@ document.getElementById('bubels').animate([
   });;
 */
 
-//console.log(window.screen.height);
 
 // Стартова властивості рухомих декоративних кіл
 circles[0].style = 'left: '+((1 - circle_height_koef * 0.3) * 100)/1.4+'%;'+
@@ -49,7 +51,43 @@ circles[1].style = 'left: '+(circle_height_koef - 1)/3 * 100+'%;'+
 scrooll_to_section('up');
 // ---------------
 
-// отлавливание всех способов перелистывания
+
+// размещаем кнупки навигации и вешаем обработчик нажатий
+for (let i = 0; i < menu_buttons.length; i++){    
+    menu_buttons[i].style = 'top: '+ (30 * (i + 1)-22)+'%';
+    menu_buttons[i].addEventListener("click", click_on_menu_button);
+    // первая кнопка сразу активна
+    if (i == 0)
+        menu_buttons[i].style = menu_buttons[i].style.cssText+'right: -20%;';
+}
+// ----------------
+
+
+// функция нажатия на кнопку навигации
+function click_on_menu_button(event){
+    for (let i = 0; i < menu_buttons.length; i++){
+        if (menu_buttons[i].childNodes[0] == event.target){
+            switch (i) {
+                case 0:{
+                    scrooll_to_section(0);
+                    break;
+                }
+                case 1:{
+                    scrooll_to_section(2);
+                    break;
+                }
+                case 2:{
+                    scrooll_to_section(sections.length - 1);
+                    break;
+                }
+            } 
+        }
+    }
+}
+// ----------------
+
+
+// отлавливание всех событий-способов перелистывания
 document.addEventListener("keydown", function(event){
     if (event.code == 'ArrowUp')
         scrooll_to_section("up")
@@ -73,6 +111,7 @@ document.addEventListener("touchend", function(event){
 })
 // ---------------
 
+
 // функция перелистывания между секциями
 function scrooll_to_section(direction){    
     // запобіжник, не дає перелистувати секції частіше ніж в 1100 мс.
@@ -81,22 +120,42 @@ function scrooll_to_section(direction){
     else if (!permission)
         return false;
     permission = false;
-    // ---------------    
+    // ---------------   
+
+    console.log('direction = ' + direction); 
 
     // змінюємо секцію на сусідню
     if (direction == "up" && curent_section != 0){
-        sections[curent_section].style = 'bottom: -100%; left: 0;';
-        sections[curent_section - 1].style = 'bottom: 0; left: 0;';
+        sections[curent_section].style = 'bottom: -100%; left: 0; opacity: 0;';
+        sections[curent_section - 1].style = 'bottom: 0; left: 0; opacity: 1;';
         curent_section -= 1;
     }
     else if (direction == "down" && curent_section != (sections.length - 1)){
-        sections[curent_section].style = 'bottom: 100%; left: 0;';
-        sections[curent_section + 1].style = 'bottom: 0; left: 0;';
+        sections[curent_section].style = 'bottom: 100%; left: 0; opacity: 0;';
+        sections[curent_section + 1].style = 'bottom: 0; left: 0; opacity: 1;';
         curent_section += 1;
+    }
+    // або листаємо до секції вказаної через кнопку навігації
+    else if (direction == 0 || direction == 2 || direction == (sections.length - 1)){
+        if (curent_section < direction){
+            for( let i = curent_section; i < direction; i++)
+                sections[i].style = 'bottom: 100%; left: 0; opacity: 0;';
+            sections[direction].style = 'bottom: 0; left: 0; opacity: 1;';
+            curent_section = direction;
+        } else if (curent_section > direction){
+            for (let i = curent_section; i > direction; i--)
+                sections[i].style = 'bottom: -100%; left: 0; opacity: 0;';
+            sections[direction].style = 'bottom: 0; left: 0; opacity: 1;';
+            curent_section = direction;
+        }      
     }
     // ---------------
 
-    // рухаємо декоративні кола
+    // рухаємо декоративні кола та кнопки навігації
+    // обнуляємо позиції кнопок 
+    for (let i = 0; i < menu_buttons.length; i++)
+        menu_buttons[i].style = menu_buttons[i].style.cssText+'right: -5%;';
+    // задаємо нові позиції для кіл та кнопок
     switch (curent_section) {
         case 0:{
             //console.log('секція перша, кільця відповідно до неї');
@@ -109,6 +168,8 @@ function scrooll_to_section(direction){
             ' top: '+((1 - circle_height_koef * 0.3) * 100)/1.2+'%;'+
             ' height: '+(window.innerHeight * (circle_height_koef - 0.1))+'px;'+
             ' width: '+(window.innerWidth * 0.55)+'px';
+
+            menu_buttons[0].style = menu_buttons[0].style.cssText+'right: -20%;';
             break;
         }
         case 1:{
@@ -122,6 +183,8 @@ function scrooll_to_section(direction){
             ' top: '+(0.8 * 100)/1.2+'%;'+
             ' height: '+(window.innerHeight * (circle_height_koef - 0.1))+'px;'+
             ' width: '+(window.innerWidth * 0.55)+'px';
+
+            menu_buttons[0].style = menu_buttons[0].style.cssText+'right: -20%;';
             break;
         }
         case 2:{
@@ -135,6 +198,8 @@ function scrooll_to_section(direction){
             ' top: '+(circle_height_koef * 100)/2*(-1)+'%;'+
             ' height: '+(window.innerHeight * circle_height_koef)+'px;'+
             ' width: '+(window.innerWidth * 0.5)+'px';
+
+            menu_buttons[1].style = menu_buttons[1].style.cssText+'right: -20%;';
             break;
         }
         case 3:{
@@ -148,6 +213,8 @@ function scrooll_to_section(direction){
             ' top: '+(circle_height_koef * 100)/2*(-1)+'%;'+
             ' height: '+(window.innerHeight * circle_height_koef)+'px;'+
             ' width: '+(window.innerWidth * (1 - circle_height_koef + 0.3))+'px';
+
+            menu_buttons[1].style = menu_buttons[1].style.cssText+'right: -20%;';
             break;
         }
         case 4:{
@@ -161,6 +228,8 @@ function scrooll_to_section(direction){
             ' top: '+(0.8 * 100)/1.5+'%;'+
             ' height: '+(window.innerHeight * circle_height_koef)+'px;'+
             ' width: '+(window.innerWidth * (1 - circle_height_koef + 0.4))+'px';
+
+            menu_buttons[1].style = menu_buttons[1].style.cssText+'right: -20%;';
             break;
         }
         case 5:{
@@ -174,6 +243,8 @@ function scrooll_to_section(direction){
             ' top: '+(0.8 * 100)/2+'%;'+
             ' height: '+(window.innerHeight * (circle_height_koef + 0.1))+'px;'+
             ' width: '+(window.innerWidth * 0.7)+'px';
+
+            menu_buttons[2].style = menu_buttons[2].style.cssText+'right: -20%;';
             break;
         }
       }    
@@ -181,7 +252,8 @@ function scrooll_to_section(direction){
 }
 // -------------------
 
-// слайдер ----------------
+
+// ініціалізація слайдера ----------------
 const swiper = new Swiper('.swiper', {
     loop: true,
     pagination: {
@@ -189,164 +261,3 @@ const swiper = new Swiper('.swiper', {
     },
 });
 //------------------------------
-
-
-
-/*                               OLD DOC
-let body = document.body;
-
-// Всплывающий зелёный фонон на больших картинках ----------------
-let big_img = body.querySelectorAll('.big_img');
-for (let i = 0; i < big_img.length; i++)
-{
-    big_img[i].addEventListener("mouseenter", Show_pop_up);
-    big_img[i].addEventListener("mouseleave", Hide_pop_up);
-}
-
-function Show_pop_up(event){
-    if (event.target.classList.contains('big_img_target_1'))
-    {
-        body.querySelector('.big_img_target_1 div').style.display = "block";
-    }
-    else if (event.target.classList.contains('big_img_target_2'))
-    {
-        body.querySelector('.big_img_target_2 div').style.display = "block";
-    }
-    else if (event.target.classList.contains('big_img_target_3'))
-    {
-        body.querySelector('.big_img_target_3 div').style.display = "block";
-    }
-    else if (event.target.classList.contains('big_img_target_4'))
-    {
-        body.querySelector('.big_img_target_4 div').style.display = "block";
-    }
-}
-function Hide_pop_up(event){
-    if (event.target.classList.contains('big_img_target_1'))
-    {
-        body.querySelector('.big_img_target_1 div').style.display = "none";
-    }
-    else if (event.target.classList.contains('big_img_target_2'))
-    {
-        body.querySelector('.big_img_target_2 div').style.display = "none";
-    } 
-    else if (event.target.classList.contains('big_img_target_3'))
-    {
-        body.querySelector('.big_img_target_3 div').style.display = "none";
-    }
-    else if (event.target.classList.contains('big_img_target_4'))
-    {
-        body.querySelector('.big_img_target_4 div').style.display = "none";
-    } 
-}
-// -------------------
-
-
-// Выезжающее меню ----------------
-let menu_nav = document.getElementById('menu');
-let menu_icon = document.getElementById('menu_icon');
-let display_status = false;
-menu_icon.addEventListener("click", Menu_controll);
-
-function Menu_controll(){    
-    //console.log(menu_nav);
-    if (!display_status) {
-        body.style.overflow = 'hidden';
-
-        display_status = true;
-        menu_nav.classList.add('hide_menu');
-        setTimeout(function(){
-            menu_nav.classList.remove('hide_menu');
-            menu_nav.classList.add('active_menu');
-            menu_nav.classList.add('show_menu');
-         }, 685);
-         setTimeout(function(){ menu_nav.classList.remove('show_menu') }, 690+690);              
-    } else {
-        body.style.overflow = 'auto';
-
-        display_status = false;
-        menu_nav.classList.add('hide_menu');
-        setTimeout(function(){
-            menu_nav.classList.remove('active_menu');
-            menu_nav.classList.remove('hide_menu');
-            menu_nav.classList.add('show_menu');
-         }, 685);
-         setTimeout(function(){ menu_nav.classList.remove('show_menu') }, 690+690);   
-    }
-}
-// -------------------
-
-
-// слайдер ----------------
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    pagination: { el: '.swiper-pagination' }
-  });
-//------------------------------
-
-
-// обработка нажатия кнопок меню ----------------
-let menu = body.querySelector('header nav ul');
-let description_unit = body.querySelector('.description');
-let work_unit = body.querySelector('.works');
-let shop_unit = body.querySelector('.shop');
-let contact_unit = body.querySelector('.contact');
-
-let menu_button_ABOUT_US = body.querySelector('ABOUT_US');
-menu.addEventListener("click", menu_button_click);
-
-function menu_button_click(event){
-    //console.log(event.target);
-    if (event.target.classList.contains('ABOUT_US'))
-    {
-        Menu_controll();
-        description_unit.scrollIntoView({block: "center", behavior: "smooth"});
-    } else if (event.target.classList.contains('WORK'))
-    {
-        Menu_controll();
-        work_unit.scrollIntoView({block: "start", behavior: "smooth"});
-    } else if (event.target.classList.contains('SHOP'))
-    {
-        Menu_controll();
-        shop_unit.scrollIntoView({block: "start", behavior: "smooth"});
-    } else if (event.target.classList.contains('CONTACT'))
-    {
-        Menu_controll();
-        contact_unit.scrollIntoView({block: "center", behavior: "smooth"});
-    }
-}
-//------------------------------
-
-
-
-// прячем/показываем панель навигации при скролинге ----------------
-window.addEventListener('scroll', test);
-let old_scroll_y = 0, current_top = -1000;
-function test(event){
-    if (innerWidth < 767){
-        if (current_top == -1000)
-            current_top = window.getComputedStyle(menu_nav).top.replace('px', '');
-        else
-            current_top = menu_nav.style.top.replace('px', '');
-
-        if (window.scrollY > old_scroll_y && current_top != -90) // крутим в низ
-        {
-            current_top -= (window.scrollY - old_scroll_y);
-            if (current_top < -90){
-                current_top = -90;
-            }
-        }
-        else if (window.scrollY < old_scroll_y && current_top != 0) // крутим вверх
-        {
-            current_top = (old_scroll_y - window.scrollY) - (current_top * -1);
-            if (current_top > 0){
-                current_top = 0;
-            }
-        }
-
-        old_scroll_y = window.scrollY;
-        menu_nav.style.top = current_top +'px';
-    }
-}
-//------------------------------
-*/
